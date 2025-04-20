cdbfd1f3525b826475c95a6fa9fdaf60db10a1e5
tejas-oza <ozatejas01@gmail.com>
2025-04-20 11:20:19 +0530

    feat(tailwind css): Install tailwind css and check it is working properly or not.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <div></div>
+      <div className="w-full h-1/2 p-10 flex items-center justify-center bg-red-200">
+        <h1 className="text-5xl text-violet-400">Hello World!</h1>
+      </div>
     </>
   );
 }
