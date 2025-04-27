c7a54d7cd4713b2acd5cccb34da93df17a4a9ec8
tejas-oza <ozatejas01@gmail.com>
2025-04-27 14:10:40 +0530

    feat(Reusable components): Create reusable components - Button, Input, Container

---
+++ Frontend/src/components/ui/Button.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { cn } from "../../lib/utils.js";
+
+const Button = ({ children, className, variant, size, ...props }) => {
+  const variantProp = {
+    base: "bg-white dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50 hover:bg-zinc-100 dark:hover:bg-zinc-800 shadow",
+    destructive:
+      "bg-red-600 dark:bg-red-700 text-zinc-50 hover:bg-red-700 dark:hover:bg-red-800 shadow-sm",
+    outline:
+      "text-zinc-950 dark:text-zinc-50 border border-zinc-300 dark:border-zinc-800 bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-800 hover:bg-zinc-100 shadow-sm",
+    ghost:
+      "bg-transparent text-zinc-950 hover:bg-zinc-100 dark:text-zinc-50 dark:hover:bg-zinc-800",
+    secondary:
+      "bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700 text-zinc-950 dark:text-zinc-50",
+  };
+
+  const sizeProp = {
+    base: "px-4 py-2 h-9",
+    sm: "h-8 rounded-md px-3 text-xs",
+    lg: "h-10 rounded-md px-4",
+    icon: "h-8 w-8",
+  };
+
+  return (
+    <button
+      className={cn(
+        "font-Inter font-semibold text-sm inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md transition-all cursor-pointer [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+        variantProp[variant],
+        sizeProp[size],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ Frontend/src/components/ui/Input.jsx
@@ -0,0 +1,29 @@
+import React, { forwardRef } from "react";
+import { cn } from "../../lib/utils";
+
+const Input = forwardRef(
+  ({ className, type = "text", label, ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-2">
+        {label && (
+          <p className="font-Inter font-semibold dark:font-medium text-sm text-zinc-950 dark:text-zinc-50">
+            {label}
+          </p>
+        )}
+        <input
+          ref={ref}
+          className={cn(
+            "font-Inter font-medium text-base border border-zinc-200 dark:border-zinc-700 rounded-md text-zinc-950 dark:text-zinc-50 placeholder:text-zinc-400 dark:placeholder:text-zinc-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-800 dark:focus-visible:ring-zinc-600 px-3 py-1 flex h-9 w-full bg-transparent file:bg-transparent file:text-sm file:font-medium file:text-zinc-500 dark:file:text-zinc-300 disabled:cursor-not-allowed",
+            className
+          )}
+          type={type}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
