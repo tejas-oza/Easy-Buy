a9f4938b3a46e0af2cf98bd7754f8cbfbbdab0ad
tejas-oza <ozatejas01@gmail.com>
2025-05-03 10:05:21 +0530

    refactor(App.jsx): Update the ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
@@ -8,12 +9,16 @@ function App() {
 
   return (
     <>
-      <section className={`${isDarkMode && "dark"} relative`}>
+      <section
+        className={`relative max-w-svw min-h-svh overflow-hidden bg-zinc-50 dark:bg-zinc-900 ${
+          isDarkMode && "dark"
+        }`}
+      >
         <NavbarContainer />
 
         <Routes>
           <Route path="/register" element={<AuthPage />} />
-          <Route path="/login" element={<AuthPage type={true} />} />
+          <Route path="/login" element={<AuthPage />} />
         </Routes>
       </section>
     </>
