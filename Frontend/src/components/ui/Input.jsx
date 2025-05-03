e792ee7bddf058c51ffb98c723e539852401fd9e
tejas-oza <ozatejas01@gmail.com>
2025-05-03 10:08:10 +0530

    refactor(Input): Update the style and ui of reusable input component

--- Frontend/src/components/ui/Input.jsx
+++ Frontend/src/components/ui/Input.jsx
@@ -1,28 +1,19 @@
 import React, { forwardRef } from "react";
 import { cn } from "../../lib/utils";
 
-const Input = forwardRef(
-  ({ className, type = "text", label, ...props }, ref) => {
-    return (
-      <div className="flex flex-col gap-2">
-        {label && (
-          <p className="font-Inter font-semibold dark:font-medium text-sm text-zinc-950 dark:text-zinc-50">
-            {label}
-          </p>
-        )}
-        <input
-          ref={ref}
-          className={cn(
-            "font-Inter font-medium text-base border border-zinc-300 dark:border-zinc-700 rounded-md text-zinc-950 dark:text-zinc-50 placeholder:text-zinc-400 dark:placeholder:text-zinc-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-800 dark:focus-visible:ring-zinc-600 px-3 py-1 flex h-9 w-full bg-transparent file:bg-transparent file:text-sm file:font-medium file:text-zinc-500 dark:file:text-zinc-300 disabled:cursor-not-allowed",
-            className
-          )}
-          type={type}
-          {...props}
-        />
-      </div>
-    );
-  }
-);
+const Input = forwardRef(({ className, type = "text", ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={cn(
+        "font-Inter text-base font-medium text-zinc-950 dark:text-zinc-50 placeholder:text-sm placeholder:text-zinc-700 dark:placeholder:text-zinc-400 px-4 py-2 border border-zinc-800 dark:border-zinc-400 rounded-lg focus-visible:outline-none disabled:cursor-not-allowed",
+        className
+      )}
+      type={type}
+      {...props}
+    />
+  );
+});
 
 Input.displayName = "Input";
 
