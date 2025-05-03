c326c82a272a79d590c8408ba86f1f2f0b72195f
tejas-oza <ozatejas01@gmail.com>
2025-05-03 15:10:00 +0530

    update(ProtectedRoute): Update protected route component

--- Frontend/src/components/Common/ProtectedRoute.jsx
+++ Frontend/src/components/Common/ProtectedRoute.jsx
@@ -3,14 +3,9 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
 const ProtectedRoute = ({ allowedRoles = [] }) => {
-  console.log(allowedRoles);
-
   const user = useSelector((state) => state.auth.user);
 
   if (!user || user === null) {
-    console.log("hi");
-    console.log(user);
-
     return <Navigate to="/login" replace />;
   }
 
