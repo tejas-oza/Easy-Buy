7099d632c33f6ea390778120a19917fab8037704
tejas-oza <ozatejas01@gmail.com>
2025-05-09 23:33:59 +0530

    feat(initial setup2): set up the initial file for the frontend of ecommerce

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Route, Routes } from "react-router";
-import NavbarContainer from "./components/Navbar/NavbarContainer";
-import AuthPage from "./pages/AuthPage/AuthPage";
 
-function App() {
-  const isDarkMode = useSelector((state) => state.global.isDarkMode);
-
-  return (
-    <>
-      <section
-        className={`relative max-w-svw min-h-svh overflow-hidden bg-zinc-50 dark:bg-zinc-900 ${
-          isDarkMode && "dark"
-        }`}
-      >
-        <NavbarContainer />
-
-        <Routes>
-          <Route path="/register" element={<AuthPage />} />
-          <Route path="/login" element={<AuthPage />} />
-        </Routes>
-      </section>
-    </>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
