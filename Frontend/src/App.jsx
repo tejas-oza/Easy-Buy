6427815cb7f23a32bb56471fa4e1c542ad999b3f
tejas-oza <ozatejas01@gmail.com>
2025-05-11 11:16:57 +0530

    feat(Basic layout setup): Setup basic layout for the app and create navbar and footer components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <section className="w-full min-h-svh overflow-hidden flex flex-col">
+      <Navbar />
+
+      <main className="flex-1">Main content</main>
+
+      <Footer />
+    </section>
+  );
 };
 
 export default App;
