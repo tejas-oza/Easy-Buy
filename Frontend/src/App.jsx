6e538d50eb3c4c514975635c18f0ad3f834bf89f
tejas-oza <ozatejas01@gmail.com>
2025-05-18 16:54:21 +0530

    feat(Dark and Light mode):  Implement dark and light mode

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const isDarkMode = useSelector((state) => state.global.isDarkMode);
+
   return (
-    <section className="w-full min-h-svh overflow-hidden flex flex-col">
+    <section
+      className={`w-full min-h-svh overflow-hidden flex flex-col ${
+        isDarkMode && "dark"
+      }`}
+    >
       <Navbar />
 
       <main className="flex-1">Main content</main>
