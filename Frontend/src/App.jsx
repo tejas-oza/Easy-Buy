502a94fb018072e048ead6ed133e4c6c44d17012
tejas-oza <ozatejas01@gmail.com>
2025-05-21 13:05:49 +0530

    update(App): Add dark class based on theme

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { useSelector } from "react-redux";
+import NavbarContainer from "./components/Navbar/NavbarContainer";
 
 const App = () => {
   const isDarkMode = useSelector((state) => state.global.isDarkMode);
 
   return (
     <section
-      className={`w-full min-h-svh overflow-hidden flex flex-col ${
+      className={`relative w-full min-h-svh overflow-hidden flex flex-col ${
         isDarkMode && "dark"
       }`}
     >
-      <Navbar />
+      <NavbarContainer />
 
       <main className="flex-1">Main content</main>
 
